d7f4a1f22f00bdab7f379c11d96b89fcc51094da
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-08 12:08:01 +0100

    Generate MIT | Apache | GNUGPL | Mozilla license

--- src/commands/license.nim
+++ src/commands/license.nim
@@ -1,10 +1,26 @@
-import os
 import tables
 import strformat
-import sequtils
+import strutils
 import fab
 import docopt
-import progress
+import run
 
 proc genLicense*(args: Table[system.string, docopt.Value]) =
-  echo "WIP"
+  
+
+  for license in @(args["<license>"]):
+    case license.toLower():
+      of "mit":
+        exec &"cp $(nimble path gen)/templates/license/mit.txt ./License.txt"
+      
+      of "apache":
+        exec &"cp $(nimble path gen)/templates/license/apache.txt ./License.txt"
+      
+      of "mozilla":
+        exec &"cp $(nimble path gen)/templates/license/mozilla.txt ./License.txt"
+      
+      of "gnugpl":
+        exec &"cp $(nimble path gen)/templates/license/gnugpl3.txt ./License.txt"
+
+      else:
+        red "sorry i don't have that license :("

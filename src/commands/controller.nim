55b0e84ecaf77762b55f0fa5314c458edbce683d
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-08 14:19:44 +0100

    Create initial WIP model & controller

---
+++ src/commands/controller.nim
@@ -0,0 +1,9 @@
+import tables
+import strformat
+import strutils
+import fab
+import docopt
+import run
+
+proc genController*(args: Table[system.string, docopt.Value]) =
+  blue "WIP"
---
+++ src/commands/model.nim
@@ -0,0 +1,9 @@
+import tables
+import strformat
+import strutils
+import fab
+import docopt
+import run
+
+proc genModel*(args: Table[system.string, docopt.Value]) =
+  blue "WIP"

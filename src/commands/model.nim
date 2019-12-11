3ad2b0e3f7e90e6735430bcf98a110e22dc9b80c
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-11 12:38:23 +0100

    Edit directory link

--- src/commands/model.nim
+++ src/commands/model.nim
@@ -6,4 +6,4 @@ import docopt
 import run
 
 proc genModel*(args: Table[system.string, docopt.Value]) =
-  exec &"cp $(nimble path gen)/templates/models/models.nim models.nim"
+  exec &"cp $(nimble path gen)/templates/mvc/models.nim models.nim"

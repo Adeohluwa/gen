719a1c9a5a238d5bc7ec9f9eef7433ebc0fde987
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-16 23:25:36 +0000

    Trim indentation

--- src/commands/model.nim
+++ src/commands/model.nim
@@ -7,42 +7,42 @@ import run
 
 proc genModel*(args: Table[system.string, docopt.Value]) =
   let text = """
-  import os
-  import norm/postgres
-  import dotenv
+import os
+import norm/postgres
+import dotenv
 
-  let env  = initDotEnv()
-  env.load()
+let env  = initDotEnv()
+env.load()
 
-  let host = getEnv("host")
-  let dbname = getEnv("db")
-  let username = getEnv("user")
-  let password = getEnv("pass")
+let host = getEnv("host")
+let dbname = getEnv("db")
+let username = getEnv("user")
+let password = getEnv("pass")
 
-  # edit existing models
-  # or add your models under the type section
-  # possible types are int | float | bool | string
-  db(host,username,password,dbname):
-    type
-      Profile* = object
-        name: string
-        pass: string
-        phone: string
-        country: string
-      
-      Review* = object
-        name: string
-        rating: int
-        comment: string 
-      
-      Suggestions* = object
-        phone: string
-        comment: string
+# edit existing models
+# or add your models under the type section
+# possible types are int | float | bool | string
+db(host,username,password,dbname):
+  type
+    Profile* = object
+      name: string
+      pass: string
+      phone: string
+      country: string
+    
+    Review* = object
+      name: string
+      rating: int
+      comment: string 
+    
+    Suggestions* = object
+      phone: string
+      comment: string
 
 
-  when isMainModule:
-    withDb:
-      createTables()
-  """
+when isMainModule:
+  withDb:
+    createTables()
+"""
   for filename in @(args["<model>"]):
     writeFile(&"{filename}", text)

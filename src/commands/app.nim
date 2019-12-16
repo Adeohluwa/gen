027a82f591c7a80d8197dd9a5062904f37a3555d
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-16 22:41:43 +0000

    Refactor app command

--- src/commands/app.nim
+++ src/commands/app.nim
@@ -5,6 +5,8 @@ import sequtils
 import fab
 import docopt
 import progress
+import run
+
 
 proc genApp*(args: Table[system.string, docopt.Value]) =
   for name in @(args["<project>"]):
@@ -12,23 +14,17 @@ proc genApp*(args: Table[system.string, docopt.Value]) =
     blue(&"Generating folder structure for {name}...")
    
     # Initial files
-    let configFiles = @["app.nimble"]
-   
+    let configFiles = @["app.nimble", "README.md"]
     #Nim Files
     let nimFiles = @["app.nm"]
-
     # MVC folder
     let mvcDir = @["src/models", "src/routes", "src/views"]
-    
     # Public directory
     let pubDir = @[ "src/public/img", "src/public/css", "src/public/js"]
- 
     # Tests
     let testDir = @[&"tests"]
     let allDirs = mvcDir & pubDir & testDir
 
-    proc run(cmd: string) =
-      discard execShellCmd cmd
 
     # start new progress bar
     var bar = newProgressBar()
@@ -40,15 +36,15 @@ proc genApp*(args: Table[system.string, docopt.Value]) =
      
     # write config files
     for eachFile in configFiles:
-      run &"cp $(nimble path gen)/templates/{eachFile} ./{name}/{eachFile}"
+      exec &"cp $(nimble path gen)/templates/{eachFile} ./{name}/{eachFile}"
     
     # write nim files
     for eachFile in nimFiles:
-      run &"cp $(nimble path gen)/templates/{eachFile} ./{name}/src/app.nim"
+      exec &"cp $(nimble path gen)/templates/{eachFile} ./{name}/src/app.nim"
 
     for i in 1..100:  
       sleep(30)
       bar.increment()
     
     bar.finish()
-    run &"cd {name} && nimble run app"
+    exec &"cd {name} && nimble run app"

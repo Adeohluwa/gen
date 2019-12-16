2458263d2f538d5000ac90c66b0b68c74f0b8492
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-16 23:26:53 +0000

    Add db insert example

--- src/commands/controller.nim
+++ src/commands/controller.nim
@@ -7,11 +7,15 @@ import run
 
 proc genController*(args: Table[system.string, docopt.Value]) =
   let text = """
-  import norm/postgres
+import norm/postgres
+
+proc createController(arg: type): type =
+  withDb:
+    newUser = User(
+      name:"sherluck holmes",
+      age: "9"
+    )
+    newUser.insert()"""
   
-  proc createController(arg: type): type =
-    withDb:
-      ... 
-  """
   for filename in @(args["<controller>"]):
     writeFile(&"{filename}",text)

5ff39079451c0dc3efcfb46d466dc95279ad9228
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-20 00:23:35 +0100

    Add BSD & CC0 License

--- src/commands/license.nim
+++ src/commands/license.nim
@@ -12,17 +12,23 @@ proc genLicense*(args: Table[system.string, docopt.Value]) =
       of "mit":
         exec &"cp $(nimble path gen)/templates/license/mit.txt ./License.txt"
       
+      of "bsd":
+        exec &"cp $(nimble path gen)/templates/license/bsd.txt ./License.txt"
+      
+      of "cc0":
+        exec &"cp $(nimble path gen)/templates/license/cc.txt ./License.txt"
+ 
       of "apache":
         exec &"cp $(nimble path gen)/templates/license/apache.txt ./License.txt"
       
       of "mozilla":
         exec &"cp $(nimble path gen)/templates/license/mozilla.txt ./License.txt"
       
-      of "gpl3":
+      of "gpl":
         exec &"cp $(nimble path gen)/templates/license/gnugpl3.txt ./License.txt"
 
-      of "unilicense":
-        exec &"cp $(nimble path gen)/templates/license/unilicense.txt ./License.txt"
+      of "unlicense":
+        exec &"cp $(nimble path gen)/templates/license/unlicense.txt ./License.txt"
       
       else:
         red "sorry i don't have that license :("

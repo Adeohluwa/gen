482fe7002d7460f0875a4ae79e8e07ca1cf55333
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-20 12:48:11 +0100

    Make docker command self contained

--- src/commands/docker.nim
+++ src/commands/docker.nim
@@ -1,9 +1,50 @@
+import sugar
 import tables
 import strformat
 import strutils
 import fab
 import docopt
-import run
+
+
+
+let dockerFile = () => writeFile(
+"Dockerfile",
+"""
+FROM nimlang/nim:onbuild
+RUN rm /etc/nginx/conf.d/*
+COPY src/nginx/proxy.conf /etc/nginx/conf.d/
+ENTRYPOINT ["./bin/app"]
+"""
+)
+
+
+let dockerCompose = () => writeFile(
+"docker-compose.yml",
+"""
+version: '3.7'
+services:
+  web:
+    image: nginx:latest
+    build: .
+    depends_on:
+      - db
+    ports:
+      - "80:80"
+      - "443:443"
+    restart: always
+  db:
+    image: postgres
+    environment:
+      POSTGRES_DB=dbname:
+      POSTGRES_USER=username:
+      POSTGRES_PASSWORD=password:
+    volumes:
+      - ./pgdata:/var/lib/postgresql/data
+    ports:
+      - '5432:5432'
+"""
+)
 
 proc genDocker*() =
-  exec &"cp $(nimble path gen)/templates/docker/* ."
+  dockerFile()
+  dockerCompose()

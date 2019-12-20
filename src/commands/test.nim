df0c65f28707d62ec2c52ab681e5490f3658e9c0
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-20 14:13:37 +0100

    Fix tests indentation

--- src/commands/test.nim
+++ src/commands/test.nim
@@ -11,6 +11,20 @@ import docopt
 
 let writeTest = (content: string) => writeFile("tRoutes.nim",content)
 
+var buildUp = """
+import unittest
+import httpclient
+
+
+
+let client = newHttpClient()
+client.headers = newHttpHeaders({"Content-Type":"application/json"})
+
+
+suite "Tests all the routes":
+"""
+
+
 
 
 proc genTest*(args: Table[system.string, docopt.Value]) =
@@ -21,19 +35,10 @@ proc genTest*(args: Table[system.string, docopt.Value]) =
     let contents = readFile(filename)
     let matchObjects = contents.findAll(pattern)
 
-    var buildUp = """
-import unittest
-import httpclient
-
-let client = newHttpClient()
-
-client.headers = newHttpHeaders({"Content-Type":"application/json"})
-"""
-
     for each in matchObjects:
       let reqMethod = each.findAll(re(r"post|get|put|patch|delete"))[0]
       let url = each.findAll(re(r"/.+"))[0].replace(":","")
-      buildUp.add(&"\n\ncheck:\n  client.{reqMethod}(\"{url}) == Http500") 
+      buildUp.add(&"\n\n\n  test \"{url}:\n      check:  client.{reqMethod}(\"{url}) == Http500") 
 
 
     writeTest(buildUp)

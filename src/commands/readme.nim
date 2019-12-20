2eb3658af644024ff63431a9ac8166a981cbe902
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-20 12:48:43 +0100

    Delete readme command

--- src/commands/readme.nim
+++
@@ -1,9 +0,0 @@
-import tables
-import strformat
-import strutils
-import fab
-import docopt
-import run
-
-proc genReadMe*() =
-  exec &"cp $(nimble path gen)/templates/readme/README.md ./README.md"

4beae18be656162f2d4f5130292c22dec33e4053
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-20 01:25:06 +0100

    Make license function self contained

--- src/commands/license.nim
+++ src/commands/license.nim
@@ -1,34 +1,98 @@
+import sugar
 import tables
-import strformat
 import strutils
+import strformat
 import fab
-import docopt
 import run
+import gara
+import docopt
+
+
+let BSD = () => writeFile(
+"License.txt",
+"""
+Copyright (c) 2017, [Company]
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the
+  following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other materials provided with the distribution.
+
+* Neither the name of the [Company] nor the names of its contributors may be used to endorse or promote
+  products derived from this software without specific prior written permission.
+
+*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*
+"""
+)
+let MIT = () => writeFile(
+"License.txt",
+"""
+MIT License
+
+Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+)
+
+let APACHE = """
+"""
+
+let GPL ="""
+"""
+
+let FAIL = () => red "sorry i do not have that license :("
+
+
+
+
+
 
 
 proc genLicense*(args: Table[system.string, docopt.Value]) =
   for license in @(args["<license>"]):
-    case license.toLower():
-      of "mit":
-        exec &"cp $(nimble path gen)/templates/license/mit.txt ./License.txt"
-      
-      of "bsd":
-        exec &"cp $(nimble path gen)/templates/license/bsd.txt ./License.txt"
-      
-      of "cc0":
-        exec &"cp $(nimble path gen)/templates/license/cc.txt ./License.txt"
- 
-      of "apache":
-        exec &"cp $(nimble path gen)/templates/license/apache.txt ./License.txt"
-      
-      of "mozilla":
-        exec &"cp $(nimble path gen)/templates/license/mozilla.txt ./License.txt"
-      
-      of "gpl":
-        exec &"cp $(nimble path gen)/templates/license/gnugpl3.txt ./License.txt"
-
-      of "unlicense":
-        exec &"cp $(nimble path gen)/templates/license/unlicense.txt ./License.txt"
-      
-      else:
-        red "sorry i don't have that license :("
+    match(license.toLower()):
+      "mit":
+        MIT()
+      "bsd":
+        BSD()
+      "cc0":
+        MIT()
+      "apache":
+        MIT()  
+      "mozilla":
+        MIT() 
+      "gpl":
+        MIT()
+      "unlicense":
+        MIT()
+      _:
+        FAIL()

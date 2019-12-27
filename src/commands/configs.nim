9c0766b77b1c46b178dd2e8b0fc3403fb23cf04f
Ade <adejumoadeoluwa@gmail.com>
2019-12-27 11:20:19 +0100

    Add .travis.yml config

--- src/commands/configs.nim
+++ src/commands/configs.nim
@@ -12,6 +12,7 @@ let dotNim = () => writeFile(
 """
 --threads:on
 -d:release
+# opt:speed
 """
 )
 
@@ -19,6 +20,33 @@ let dotNim = () => writeFile(
 let dotTravis = () => writeFile(
 ".travis.yml",
 """
+os:
+  - linux
+dist: trusty
+
+language: c
+
+cache:
+  directories:
+    - "$HOME/.choosenim"
+
+install:
+  - export CHOOSENIM_CHOOSE_VERSION="1.0.4"
+  - |
+      curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
+      sh init.sh -y
+  - export PATH=$HOME/.nimble/bin:$PATH
+
+before_script:
+  - set -e
+  - set -x
+
+script:
+  #library - nimble install -y
+  #app - nimble run app
+
+notifications:
+  email: false # noisy
 """
 )
 
